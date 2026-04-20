bf20416c7ae8425372a919912f77fb430416bb41
moctar <niangmoctar172@gmail.com>
2026-04-20 15:59:07 +0000

    quelques modifications

--- Backend/src/models/user.models.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-const userchema = new mongoose.Schema({nom: String, prenom: String, email: { type: String, unique: true}, password: String}, {timestamps: true});
-
-module.exports = mongoose.model('user', userchema);
